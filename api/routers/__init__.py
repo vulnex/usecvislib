d10cff79be9d51db58e3a91aa61db79ecd76a596
VULNEX <git@vulnex.com>
2026-02-24 15:18:25 +0100

    Add VULNEX comment header to api/routers/__init__.py

---
+++ api/routers/__init__.py
@@ -0,0 +1,12 @@
+#
+# VULNEX -Universal Security Visualization Library-
+#
+# File: routers/__init__.py
+# Author: Simon Roses Femerling
+# Created: 2025-01-01
+# Last Modified: 2026-02-24
+# Version: 0.3.4
+# License: Apache-2.0
+# Copyright (c) 2025 VULNEX. All rights reserved.
+# https://www.vulnex.com
+#
